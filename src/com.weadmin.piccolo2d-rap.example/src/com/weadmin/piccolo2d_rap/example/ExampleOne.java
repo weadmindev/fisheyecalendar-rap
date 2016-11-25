4fdccbfa26ed5342c5096b2919a264c40305c189
yangchuibao <chuibao.yang@dragonflow.com>
2016-11-25 10:32:27 +0800

    provide the interface for the line chart data.

--- src/com.weadmin.piccolo2d-rap.example/src/com/weadmin/piccolo2d_rap/example/ExampleOne.java
+++ src/com.weadmin.piccolo2d-rap.example/src/com/weadmin/piccolo2d_rap/example/ExampleOne.java
@@ -1,5 +1,8 @@
 package com.weadmin.piccolo2d_rap.example;
 
+import java.util.ArrayList;
+
+import org.eclipse.rap.json.JsonObject;
 import org.eclipse.rap.rwt.application.AbstractEntryPoint;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -8,12 +11,33 @@ import com.weadmin.piccolo2d_rap.Piccolo2dJS;
 
 public class ExampleOne extends AbstractEntryPoint{
 
+	private static final long serialVersionUID = 1L;
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	protected void createContents(Composite parent) {
 		parent.setLayout(null);
 		Piccolo2dJS pjs = new Piccolo2dJS(parent, SWT.NONE);
 		pjs.setBounds(20, 0, 1000, 600);
 		pjs.showText("Hello World!");
+		JsonObject json = null;
+		ArrayList list = new ArrayList();
+		for(int i=1;i<31;i++){
+			for(int j=0;j<24;j++){
+				json = new JsonObject();
+				json.add("savetime", "2016-11-"+(i<10?"0"+i:i) + " " +getRandom(24)+":"+getRandom(60)+":"+getRandom(60));
+				json.add("package", getRandom(100));
+				json.add("retime", new java.text.DecimalFormat("#.##").format((double)(Math.random())));
+				list.add(json);
+			}
+		}
+		pjs.showList(list);
 	}
 
+	public static String getRandom(int t){
+		int i = (int) (Math.random()*t);
+		String s = (i<10?"0"+i:i+"");
+		return s;
+	}
+	
 }
--- src/com.weadmin.piccolo2d-rap/src/com/weadmin/piccolo2d_rap/Piccolo2dJS.java
+++ src/com.weadmin.piccolo2d-rap/src/com/weadmin/piccolo2d_rap/Piccolo2dJS.java
@@ -1,7 +1,8 @@
 package com.weadmin.piccolo2d_rap;
 
 import java.util.ArrayList;
-
+import java.util.List;
+import org.eclipse.rap.json.JsonArray;
 import org.eclipse.rap.json.JsonObject;
 import org.eclipse.swt.widgets.Composite;
 
@@ -13,7 +14,7 @@ public class Piccolo2dJS extends SVWidgetBase{
 	private static final long serialVersionUID = -7580109674486263430L;
 
 	public Piccolo2dJS(Composite parent, int style) {
-		super(parent, style); 
+		super(parent, style);
 	}
 
 	//show text
@@ -22,7 +23,40 @@ public class Piccolo2dJS extends SVWidgetBase{
 		parameters.add("text", text);
 		super.callRemoteMethod("showText", parameters);
 	}
+	
+	public void showList(List<JsonObject> list){
+		super.callRemoteMethod("showList", dealWithData(list));
+	}
 
+	public static JsonObject dealWithData(List<JsonObject> list){
+		JsonObject data = new JsonObject();
+		String day,date = null;
+		double x = 0;
+		JsonArray temp_package_array,temp_retime_array = null;
+		JsonObject temp = null;
+		JsonObject jsons = new JsonObject();
+		for(JsonObject json:list){
+			date = json.get("savetime").asString();
+			day = date.substring(8, 9).equals("0")?date.substring(9, 10):date.substring(8, 10);
+			x = Double.valueOf(date.substring(11, 13))*24 + Double.valueOf(date.substring(14, 16));
+			temp_package_array = new JsonArray();
+			temp_retime_array = new JsonArray();
+			temp_package_array.add(x).add(Double.valueOf(json.get("package").asString()));
+			temp_retime_array.add(x).add(Double.valueOf(json.get("retime").asString()));
+			temp = new JsonObject();
+			temp.add("savetime", date);
+			temp.add("package", temp_package_array);
+			temp.add("retime", temp_retime_array);
+			if (jsons.get(day)!=null) {
+				jsons.set(day, ((JsonArray)jsons.get(day)).add(temp));
+			}else{
+				jsons.add(day, new JsonArray().add(temp));
+			}
+		}
+		data.add("data", jsons);
+		return data;
+	}
+	
 	@Override
 	protected void handleSetProp(JsonObject properties) {
 
@@ -49,9 +83,7 @@ public class Piccolo2dJS extends SVWidgetBase{
 		res.add(new CustomRes("esl.js", true, false));
 		res.add(new CustomRes("zrender.js", true, false));
 		res.add(new CustomRes("jquery.js", true, false));
-		res.add(new CustomRes("echarts.min.js", true, false));
 		res.add(new CustomRes("piccolo2d.js", true, false));
-		res.add(new CustomRes("detailCharts.js", true, false));
 		res.add(new CustomRes("fishEyeCalendar.js", true, false));
 		res.add(new CustomRes("handler.js", true, false));
 		return res;
