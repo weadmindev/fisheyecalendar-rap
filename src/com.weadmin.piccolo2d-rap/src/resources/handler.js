118f50fc36a8b1e403361bdfc860b90fe36f3e2a
yangchuibao <chuibao.yang@dragonflow.com>
2016-11-28 18:02:12 +0800

    optimizing the data.

--- src/com.weadmin.piccolo2d-rap/src/resources/handler.js
+++ src/com.weadmin.piccolo2d-rap/src/resources/handler.js
@@ -12,7 +12,7 @@ var PICCOLO2D_BASEPATH = "rwt-resources/piccolo2djs/";
 
 		destructor : "destroy",
 		methods : ['showText','showList'],
-		properties : [ "size",'data'],
+		properties : [ "size",'date'],
 		events:[]
 
 	});
@@ -134,6 +134,11 @@ var PICCOLO2D_BASEPATH = "rwt-resources/piccolo2djs/";
 		// 	});
 		// 	parent.append($ele[0]);
 		// },
+		setDate:function(obj){
+			this._date = obj.date;
+			this._currentDay = obj.currentDay;
+			console.log('date:',obj);
+		},
 		showList:function(obj){
 			console.log('showList:',obj);
 			this.dataList = obj.data;
